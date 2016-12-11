d477a38ad09f3315e6ba7a4c3977ce685a117cb1
marcos <marcos.perez@est.fib.upc.edu>
2016-12-11 20:22:20 +0100

    graph has to be centered

--- www/src/modules/directives/chart-shares/component.js
+++ www/src/modules/directives/chart-shares/component.js
@@ -1,12 +1,6 @@
 'use strict';
-function ChartSharesCtrl($rootScope) {
-  $rootScope.$emit("heu");
+function ChartSharesCtrl($scope) {
 
-  this.$onInit = function() {
-    this.mappedData[0] = this.entity.founders.reduce(add,0) || 0;
-    this.mappedData[1] = this.entity.investors.reduce(add,0) || 0;
-    this.mappedData[2] = this.entity.employees.reduce(add,0) || 0;
-  };
 
   function add (a,b) {
     return (a.value || 0) + (b.value || 0);
@@ -14,14 +8,31 @@ function ChartSharesCtrl($rootScope) {
 
   var ctrl = this;
 
-  $rootScope.$watch('')
+  $scope.$watch("$ctrl.entity.founders", function () {
+    console.log("ControllerAs founders"); // Triggers once on init
+    mapValues(ctrl);
+  },true);
+  $scope.$watch("$ctrl.entity.employees", function () {
+    console.log("ControllerAs employees"); // Triggers once on init
+    mapValues(ctrl);
+  },true);
+  $scope.$watch("$ctrl.entity.investors", function () {
+    console.log("ControllerAs investors"); // Triggers once on init
+    mapValues(ctrl);
+  },true);
+
+  function mapValues(context){
+    context.mappedData[0] = context.entity.founders.reduce(add,0) || 0;
+    context.mappedData[1] = context.entity.investors.reduce(add,0) || 0;
+    context.mappedData[2] = context.entity.employees.reduce(add,0) || 0;
+  }
+
   ctrl.labels = ["Founders", "Investors", "Employees" ];
 
   ctrl.mappedData = [];
-
 }
 
-ChartSharesCtrl.$inject = ["$rootScope"];
+ChartSharesCtrl.$inject = ["$scope"];
 
 module.exports = /* @NgInject */
   {
