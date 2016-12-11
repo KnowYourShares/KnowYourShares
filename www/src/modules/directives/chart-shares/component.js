f5ce74b14ebc603d33f666170d5ea7cfe802025c
marcos <marcos.perez@est.fib.upc.edu>
2016-12-11 21:31:40 +0100

    aligned button

--- www/src/modules/directives/chart-shares/component.js
+++ www/src/modules/directives/chart-shares/component.js
@@ -7,24 +7,44 @@ function ChartSharesCtrl($scope) {
   }
 
   var ctrl = this;
+  ctrl.group = true;
+
 
   $scope.$watch("$ctrl.entity.founders", function () {
-    mapValues(ctrl);
+    ctrl.mapValues();
   },true);
   $scope.$watch("$ctrl.entity.employees", function () {
-    mapValues(ctrl);
+    ctrl.mapValues();
   },true);
   $scope.$watch("$ctrl.entity.investors", function () {
-    mapValues(ctrl);
+    ctrl.mapValues();
   },true);
 
-  function mapValues(context){
-    context.mappedData[0] = context.entity.founders.reduce(add,{value:0}).value || 0;
-    context.mappedData[1] = context.entity.investors.reduce(add,{value:0}).value || 0;
-    context.mappedData[2] = context.entity.employees.reduce(add,{value:0}).value || 0;
+  ctrl.mapValues = function (){
+    ctrl.labels = [];
+    ctrl.mappedData = [];
+    if(ctrl.group){
+      ctrl.labels = ["Founders", "Investors", "Employees" ];
+      ctrl.mappedData[0] = ctrl.entity.founders.reduce(add,{value:0}).value || 0;
+      ctrl.mappedData[1] = ctrl.entity.investors.reduce(add,{value:0}).value || 0;
+      ctrl.mappedData[2] = ctrl.entity.employees.reduce(add,{value:0}).value || 0;
+    }else{
+      ctrl.entity.founders.forEach(function(o){
+        ctrl.labels.push(o.name);
+        ctrl.mappedData.push(o.value);
+      });
+      ctrl.entity.investors.forEach(function(o){
+        ctrl.labels.push(o.name);
+        ctrl.mappedData.push(o.value);
+      });
+      ctrl.entity.employees.forEach(function(o){
+        ctrl.labels.push(o.name);
+        ctrl.mappedData.push(o.value);
+      });
+
+    }
   }
 
-  ctrl.labels = ["Founders", "Investors", "Employees" ];
 
   ctrl.mappedData = [];
 }
