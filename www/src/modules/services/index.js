f1699141809fc6c2d4babfe9850eebfd467c1cec
Suufian <soufian.ben.bouker@est.fib.upc.edu>
2016-12-12 17:41:15 +0100

    Services, first try.

--- www/src/modules/services/index.js
+++ www/src/modules/services/index.js
@@ -1,5 +1,108 @@
 'use strict';
 
-module.exports =
-  angular.module('app.component.services', []);
+var createBusiness = function() {
+  // Configuration and default parameters
+  var params = {};
+  var baseUrl = '';
+  var serviceBaseUrl = '';
+
+  this.config = function(config) {
+    params = angular.copy(config);
+    baseUrl = params.baseUrl || baseUrl;
+    delete params.baseUrl;
+  };
+
+  this.$get = function($resource) {
+    // Allowed operations
+    var actions = {
+      startBusiness: {
+        // TODO: -------------- En esta URL quizás falta un ID? ------------
+        url: baseUrl + serviceBaseUrl,
+        method: 'POST'
+      }
+    };
+    return $resource(baseUrl + serviceBaseUrl, null, actions);
+  };
+};
+
+var saveBusiness = function() {
+  // Configuration and default parameters
+  var params = {};
+  var baseUrl = '';
+  var serviceBaseUrl = '';
+
+  this.config = function(config){
+    params = angular.copy(config);
+    baseUrl = params.baseUrl || baseUrl;
+    delete params.baseUrl;
+  };
+
+  this.$get = function($resource) {
+    // Allowed operations
+    var actions = {
+      save: {
+        url: baseUrl + serviceBaseUrl + ':businessId',
+        method: 'PUT'
+      }
+    };
+    return $resource(baseUrl + serviceBaseUrl, null, actions);
+  };
+};
+
+var getBusiness = function() {
+  // Configuration and default parameters
+  var params = {};
+  var baseUrl = '';
+  var serviceBaseUrl = '';
+
+  this.config = function(config) {
+    params = angular.copy(config);
+    baseUrl = params.baseUrl || baseUrl;
+    delete params.baseUrl
+  };
+
+  this.$get = function($resource) {
+    // Allowed operations
+    var actions = {
+      get: {
+        url: baseUrl + serviceBaseUrl + ':businessId',
+        method: 'GET'
+      }
+    };
+    return $resource(baseUrl + serviceBaseUrl,null,actions);
+  };
+};
+
+var deleteBusiness = function() {
+  // Configuration and default parameters
+  var params = {};
+  var baseUrl = '';
+  var serviceBaseUrl = '';
+
+  this.config = function(config) {
+    params = angular.copy(config);
+    baseUrl = params.baseUrl || baseUrl;
+    delete params.baseUrl;
+  };
+
+  this.$get = function($resource) {
+    // Allowed operations
+    var actions = {
+      remove: {
+        url: baseUrl + serviceBaseUrl + ':businessId',
+        method: 'DELETE'
+      }
+    };
+    return $resource(baseUrl + serviceBaseUrl, null, actions);
+  };
+};
+
+module.exports = {
+  // TODO: ------- La siguiente linea es necesaria? -------
+  //angular.module('app.component.services', []),
   //.factory('fooService', require('./fooService'));
+  createBusiness: createBusiness,
+  getBusiness: getBusiness,
+  saveBusiness: saveBusiness,
+  deleteBusiness: deleteBusiness
+};
