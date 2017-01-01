b8735b401c1a6faf7d667fddc7d0f4d8dc753300
marcos <marcos.perez@est.fib.upc.edu>
2017-01-01 17:16:19 +0100

    added check into home controller for an given code; the resource is created before entering the view

--- www/src/modules/app/filters/filters.controller.js
+++ www/src/modules/app/filters/filters.controller.js
@@ -1,93 +1,85 @@
 'use strict';
 
 module.exports = /*@ngInject*/
-  function filtersController($scope, $rootScope, createBusiness, putBusiness, getBusiness, $state, $location, $mdToast, clipboard) {
-
-
-    var data = {
-      companyValue: 3000,
-      founders: [],
-      employees: [],
-      investors: [],
-      rounds: []
-    };
-
-    initialize();
+  function filtersController($scope, $rootScope, createBusiness, putBusiness, getBusiness, $state, $location, $mdToast, clipboard, $mdDialog) {
 
     function initialize() {
+      var data = {
+        companyValue: 3000,
+        founders: [],
+        employees: [],
+        investors: [],
+        rounds: []
+      };
 
-      if($state.params.id !== "") {
-        getBusiness.get({businessId: $state.params.id}).$promise.then(function (data) {
-          $scope.data = data.data;
+      if ($state.params.id !== "") {
+        getBusiness.get({
+          businessId: $state.params.id
+        }).$promise.then(function(response) {
+          $scope.data = response.data;
         });
+      } else {
+        $scope.data = data;
       }
 
-      $scope.data = data;
       $scope.host = 'localhost:8080/';
       $scope.businessPath = 'business/';
-
-      $scope.createRound = function () {
-        console.log('Create round');
-        var newRound = {};
-        if (!data.rounds || !data.rounds.length) {
-          newRound.name = "Milestone 1";
-          newRound.preMoney = data.companyValue;
-          newRound.moneyRaised = 1000000;
-          newRound.postMoney = newRound.preMoney + newRound.moneyRaised;
-          newRound.founders = data.founders;
-          newRound.investors = data.investors;
-          newRound.employees = data.employees;
-          data.rounds = [];
-        } else {
-          var lastRound = data.rounds[data.rounds.length - 1];
-          newRound.name = "Milestone " + (data.rounds.length + 1);
-          newRound.preMoney = lastRound.postMoney;
-          newRound.moneyRaised = 1000000;
-          newRound.postMoney = newRound.preMoney + newRound.moneyRaised;
-          newRound.founders = lastRound.founders;
-          newRound.investors = lastRound.investors;
-          newRound.employees = lastRound.employees;
-        }
-        data.rounds[data.rounds.length] = angular.copy(newRound);
-      };
-
-      $scope.removeRound = function (index) {
-        if (data.rounds.length > 1) {
-          data.rounds.splice(index, 1);
-        }
-      };
-
+      $scope.token = '/token'
       $scope.selectedIndex = 0;
 
       if (!clipboard.supported) {
-          console.log('Sorry, copy to clipboard is not supported');
+        console.log('Sorry, copy to clipboard is not supported');
       }
-
-      $scope.copyToClipboard = function () {
-
-          var path = $scope.host + $scope.businessPath + $scope.data._id;
-          if(!$scope.readOnly) {
-            path += '/token';
-          }
-          clipboard.copyText(path);
-
-          $mdToast.show($mdToast.simple().textContent('Link copied to clipboard!'));
-      };
-
-      $scope.save = function save() {
-          if(!$scope.data._id){
-              createBusiness.startBusiness($scope.data).$promise.then(function (data) {
-                $scope.data = data;
-                $mdToast.show($mdToast.simple().textContent('Changes saved'));
-                $location.path('/business/' + data._id);
-              });
-          }
-          else{
-            putBusiness.save({businessId: $scope.data._id}, $scope.data).$promise.then(function (data) {
-              $mdToast.show($mdToast.simple().textContent('Changes saved.'));
-              $scope.data = data;
-            });
-          }
-      };
     }
+
+    $scope.createRound = function() {
+      console.log('Create round');
+      var newRound = {};
+      if (!$scope.data.rounds || !$scope.data.rounds.length) {
+        newRound.name = "Milestone 1";
+        newRound.preMoney = $scope.data.companyValue;
+        newRound.moneyRaised = 1000000;
+        newRound.postMoney = newRound.preMoney + newRound.moneyRaised;
+        newRound.founders = $scope.data.founders;
+        newRound.investors = $scope.data.investors;
+        newRound.employees = $scope.data.employees;
+        $scope.data.rounds = [];
+      } else {
+        var lastRound = $scope.data.rounds[$scope.data.rounds.length - 1];
+        newRound.name = "Milestone " + ($scope.data.rounds.length + 1);
+        newRound.preMoney = lastRound.postMoney;
+        newRound.moneyRaised = 1000000;
+        newRound.postMoney = newRound.preMoney + newRound.moneyRaised;
+        newRound.founders = lastRound.founders;
+        newRound.investors = lastRound.investors;
+        newRound.employees = lastRound.employees;
+      }
+      $scope.data.rounds[$scope.data.rounds.length] = angular.copy(newRound);
+    };
+
+    $scope.removeRound = function(index) {
+        $scope.data.rounds.splice(index, 1);
+    };
+
+    $scope.copyToClipboard = function() {
+      var path = $scope.host + $scope.businessPath + $scope.data._id;
+      if (!$scope.readOnly) {
+        path += $scope.token;
+      }
+      clipboard.copyText(path);
+      $mdToast.show($mdToast.simple().textContent('Link copied to clipboard!'));
+    };
+
+    $scope.save = function save() {
+        putBusiness.save({
+          businessId: $scope.data._id
+        }, $scope.data).$promise.then(function(data) {
+          $mdToast.show($mdToast.simple().textContent('Changes saved.'));
+          $scope.data = data;
+        },function() {
+          $mdToast.show($mdToast.simple().textContent('Error saving.'));
+        });
+    };
+
+    initialize();
   };
--- www/src/modules/app/home/home.controller.js
+++ www/src/modules/app/home/home.controller.js
@@ -1,12 +1,24 @@
 'use strict';
 
 module.exports = /*@ngInject*/
-  function homeController($scope, $state) {
+  function homeController($scope, $state, createBusiness, getBusiness) {
 
     $scope.getBusiness = getObject;
 
     function getObject() {
-      $state.go('app.filters.location', {id: $scope.code});
+      if($scope.code){
+        return getBusiness.get({
+          businessId: $scope.code
+        }).$promise.then(function(response) {
+          $state.go('app.filters.location', {id: $scope.code});
+        },function () {
+          //TODO ADD ERROR TO FORM
+          console.log('ERROR 404');
+        });
+      }
+      
+      createBusiness.startBusiness({}).$promise.then(function(data) {
+        $state.go('app.filters.location', {id: data._id});
+      });
     }
-
   };
