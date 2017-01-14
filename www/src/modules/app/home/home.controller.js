d9db87f4bb9506969fc81346516fa8bb8be990c2
Daniel Dominguez <daniel.dominguez.losa@est.fib.upc.edu>
2017-01-14 20:33:35 +0100

    differentiate between start with code and without

--- www/src/modules/app/home/home.controller.js
+++ www/src/modules/app/home/home.controller.js
@@ -5,31 +5,38 @@ module.exports = /*@ngInject*/
 
     $scope.getBusiness = getObject;
 
-    function getObject() {
-      if ($scope.code) {
-        var obj = {
-          id: $scope.code,
-          password: $scope.password
-        };
-
-        console.log('object ', obj);
-        return getBusiness.get(obj).$promise.then(
-          function(response) {
-            $state.go('app.filters.location', obj);
-          },
-          function() {
-            //TODO IF WRONG PASSWORD
-            //$scope.codeInput.codePass.$setValidity("incorrect", false);
-            $scope.codeInput.codeNumber.$setValidity("notFound", false);
-            console.log('ERROR 404');
-          });
+    function getObject(withCode) {
+      if (withCode) {
+        $scope.codeInput.$setSubmitted();
+        if(!$scope.code) {
+          $scope.codeInput.codeNumber.$setValidity("required",false);
+          $scope.codeInput.codeNumber.$setValidity("notFound",true);
+        }
+        else {
+          $scope.codeInput.codeNumber.$setValidity("required",true);
+          $scope.codeInput.codeNumber.$setValidity("notFound",true);
+          var obj = {
+            id: $scope.code,
+            password: $scope.password
+          };
+          console.log('object ', obj);
+          return getBusiness.get(obj).$promise.then(
+            function(response) {
+              $state.go('app.filters.location', obj);
+            },
+            function() {
+              $scope.codeInput.codeNumber.$setValidity("notFound", false);
+              console.log('ERROR 404');
+            });
+        }
       }
-
-      createBusiness.startBusiness({}).$promise.then(function(data) {
-        $state.go('app.filters.location', {
-          id: data._id,
-          password: data.password
+      else {
+        createBusiness.startBusiness({}).$promise.then(function(data) {
+          $state.go('app.filters.location', {
+            id: data._id,
+            password: data.password
+          });
         });
-      });
+      }
     }
   };
